cf58aa8959571164c8ad6b5cf60f76126b993f08
vpast <vpast.dev@gmail.com>
2023-04-13 12:57:25 +0400

    form get data rework + field check

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,47 +1,56 @@
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
   const navigate = useNavigate();
 
   const loginHandler = (event) => {
     event.preventDefault();
+    var data = new FormData(event.target);
 
+    var email = data.get('email').trim();
+    var password = data.get('password').trim();
+    console.log(email, password);
     var loginData = {
-      email: event.target.email.value,
-      password: event.target.password.value,
+      email: email,
+      password: password,
     };
 
-    if (loginData.email === '' || loginData.password === '') {
-      document.getElementById('isFieldEmpty').style.display = 'block'
-    } else {
-      document.getElementById('isFieldEmpty').style.display = 'none'
-      
+    if (loginData.email !== '' || loginData.password !== '') {
       fetch('http://localhost:5000/users/login', {
         method: 'POST',
         body: JSON.stringify(loginData),
         headers: {
           'Content-Type': 'application/json',
-        }
-      }).then(response => {
+        },
+      }).then((response) => {
         if (response.status === 200) {
-          console.log(response.status)
-          navigate('/board')
+          console.log(response.status);
+          // navigate('/board')
         }
-      }) 
-      
+      });
     }
-    
     // console.log(loginData.email, loginData.password);
   };
-  
+
   return (
     <>
       <div className='loginPosition'>
         <form onSubmit={loginHandler} className='loginForm'>
-          <input type='email' name='email' placeholder='email'></input>
-          <input type='password' name='password' placeholder='password'></input>
+          <input
+            type='email'
+            name='email'
+            placeholder='email'
+            required
+            pattern='\S(.*\S)?'
+          ></input>
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            required
+            pattern='\S(.*\S)?'
+          ></input>
           <input type='submit' value='Login'></input>
-          <p className='isFieldEmpty' id='isFieldEmpty'>Fill all field's please.</p>
           <p>
             Not registered?{' '}
             <a href='/register' className='link'>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,40 +1,59 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const navigate = useNavigate();
 
   const registerHandler = (event) => {
     event.preventDefault();
+    var data = new FormData(event.target);
+
+    var email = data.get('email').trim();
+    var password = data.get('password').trim();
+
     var registerData = {
-      email: event.target.email.value,
-      password: event.target.password.value,
+      email: email,
+      password: password,
     };
-    
-    if (registerData.email === '' || registerData.password === '') {
-      document.getElementById('isFieldEmpty').style.display = 'block'
-      return
-    } else {
-      document.getElementById('isFieldEmpty').style.display = 'none'
+
+    if (registerData.email !== '' || registerData.password !== '') {
       fetch('http://localhost:5000/users/register', {
         method: 'POST',
         body: JSON.stringify(registerData),
         headers: {
           'Content-Type': 'application/json',
+        },
+      }).then((response) => {
+        if (response.status === 201) {
+          console.log(response.status);
+          navigate('/board')
         }
-      })
-      // navigate('/')
+      });
     }
-
   };
 
   return (
     <>
       <div className='loginPosition'>
-        <form onSubmit={registerHandler} className='loginForm' id='registerForm'>
-          <input type='email' name='email' placeholder='email'></input>
-          <input type='password' name='password' placeholder='password'></input>
+        <form
+          onSubmit={registerHandler}
+          className='loginForm'
+          id='registerForm'
+        >
+          <input
+            type='email'
+            name='email'
+            placeholder='email'
+            required
+            pattern='\S(.*\S)?'
+          ></input>
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            required
+            pattern='\S(.*\S)?'
+          ></input>
           <input type='submit' value='Register'></input>
-          <p className='isFieldEmpty' id='isFieldEmpty'>Fill all field's please.</p>
           <p>
             Already registered?{' '}
             <a href='/' className='link'>
